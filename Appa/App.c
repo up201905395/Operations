95724515281bd8b2bcd7895241746a57a07bd593
up201905395 <up201905395@fe.up.pt>
2022-10-01 20:18:37 +0100

    done changes

--- Appa/App.c
+++ Appa/App.c
@@ -4,6 +4,6 @@ public static void main(String[] args) {
 Scanner scanner = new Scanner(System.in);
 int a = scanner.nextInt();
 int b = scanner.nextInt();
-System.out.println(a + b);
+System.out.println("Result: ",a + b);
  }
 }
